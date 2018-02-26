16eb3cf6ea726501e4252621ac8a781ab9ecc611
BIT Student <classroom@bgit.rs>
2018-02-26 17:59:53 +0100

    week 5 JS in browser

---
+++ week5/first.js
@@ -0,0 +1,53 @@
+var hello = function (message) {
+    console.log(message)
+    };
+hello("Hello world again");
+
+var info = function(){
+    console.log(navigator.userAgent);
+    console.log(navigator.appVersion);
+    console.log(navigator.vendor);
+
+    
+    }
+info()
+
+var isOnline = function () {
+    if(navigator.onLine == true){
+        console.log("online");
+    } else {console.log("offline");
+    }
+    
+}
+
+isOnline();
+
+var screenSize = function(){
+    console.log(screen.availHeight);
+    console.log(screen.availWidth);
+    console.log(screen.height);
+    console.log(screen.width);
+}
+
+screenSize();
+
+
+var loc = function(){
+    console.log(location.href);
+    console.log(location.host);
+    console.log(location.protocol);
+    console.log(location.search);
+   }
+
+loc();
+
+
+//location.href = 'https://www.google.rs/';
+
+
+// var locStor = function(key, value) {
+//     localStorage.setItem(key, value);
+//     sessionStorage.setItem(key, value);
+// }
+
+// locStor('kljuc', 'vrednost');
