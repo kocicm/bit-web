ce77cb008dfb2b700cf2f6f5be9232c9fa89ac1e
BIT Student <classroom@bgit.rs>
2018-03-01 18:08:02 +0100

    super mario

---
+++ week5/Super Mario/main.js
@@ -0,0 +1,32 @@
+var backgroundPosition = 0;
+var timer;
+
+function runMario() {
+    document.querySelector('#run').setAttribute('style', 'display: block');
+    document.querySelector('#stand').setAttribute('style', 'display: none');
+
+    timer = setInterval(function() {
+        backgroundPosition -= 10;
+        document.querySelector('body').setAttribute('style', 'background-position-x:' + backgroundPosition + 'px');
+    }, 50);
+}
+
+function stopMario() {
+    document.querySelector('#stand').setAttribute('style', 'display: block');
+    document.querySelector('#run').setAttribute('style', 'display: none');
+    clearInterval(timer);
+}
+
+
+document.querySelector('body').addEventListener('keydown', function (event) {
+    if (event.keyCode === 39) {
+        runMario();
+    }
+});
+
+document.querySelector('body').addEventListener('keyup', function (event) {
+    if (event.keyCode === 39) {
+        stopMario();
+    }
+}
+);
